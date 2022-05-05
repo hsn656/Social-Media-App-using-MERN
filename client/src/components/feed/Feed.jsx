f4e52f97360895df18051d7951a87d852d715f92
Hassan Mohamed <hassan.mhmd656@gmail.com>
2022-05-05 07:54:31 +0200

    adding feed share component

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import Share from "../share/Share";
+import "./feed.css";
 
 export default function Feed() {
-  return <div>Feed</div>;
+  return (
+    <div className="feed">
+      <div className="feedWrapper">
+        <Share />
+      </div>
+    </div>
+  );
 }
